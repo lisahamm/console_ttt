4b089f09b8841fe4e95ac52473597af7ff8bc27e
Lisa Hamm <lisalhamm@gmail.com>
2015-03-15 22:24:43 -0500

    Working tic tac toe WIP

--- lib/tic_tac_toe/console_client.rb
+++ lib/tic_tac_toe/console_client.rb
@@ -1,17 +1,48 @@
 module ConsoleTTT
   class ConsoleClient
-    attr_accessor :game, :io, :view
+    attr_accessor :io, :game, :view, :setup_options
 
-    def initialize(game, io, view)
-      @game = game
+    def initialize(io)
       @io = io
-      @view = view
+      @setup_options = get_setup_options
+      @game = setup_game(setup_options)
+      @view = BoardView.new(game.board, io)
     end
 
-    def game_runner
+    def setup_game(setup_options)
+      TicTacToe::Game.new(setup_options[:player1_mark], setup_options[:player2_mark], setup_options[:player1_mark])
+    end
+
+    def get_setup_options
+      setup_options = {}
+
+      io.output("Please choose your mark (X or O). Enter 1 for X or 2 for O:")
+      mark = get_player_mark(io)
+
+      io.output("Do you want to go first or second? Enter 1 or 2:")
+      position = get_player_order(io)
+
+      io.output("Do you want to play against the computer? Enter 1 for yes or 2 for no:")
+      computer_opponent_mark = get_computer_opponent_mark(io, mark)
+
+      player1_mark, player2_mark = mark, get_opponent(mark) if position == 1
+      player1_mark, player2_mark = get_opponent(mark), mark if position == 2
+
+      setup_options[:player1_mark] = player1_mark
+      setup_options[:player2_mark] = player2_mark
+      setup_options[:computer_opponent_mark] = computer_opponent_mark
+
+      setup_options
+    end
+
+    def play!
       while game.in_progress?
-        move = get_move(io)
-        game.take_turn(move)
+        if game.current_player_mark == setup_options[:computer_opponent_mark]
+          game.take_turn(game.generate_ai_move)
+        else
+          move = get_move(io)
+          game.take_turn(move)
+        end
         display_board
       end
     end
@@ -29,6 +60,29 @@ module ConsoleTTT
 
     def display_board
       view.display
+    end
+
+    def get_player_mark(io)
+      answer = get_user_input(io).chomp.to_i
+      answer == 1 ? "X" : "O"
+    end
+
+    def get_player_order(io)
+      get_user_input(io).chomp.to_i
+    end
+
+    def computer_opponent?(io)
+      answer = get_user_input(io).chomp.to_i
+    end
+
+    def get_computer_opponent_mark(input_number, mark)
+      answer = get_user_input(io).chomp.to_i
+      return nil if answer == 2
+      return get_opponent(mark)
+    end
+
+    def get_opponent(mark)
+      mark == "X" ? "O" : "X"
     end
   end
 end
