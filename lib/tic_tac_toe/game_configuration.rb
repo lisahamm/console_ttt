f9850b07e3390e189a9875c743a14c1f8b211daa
Lisa Hamm <lisalhamm@gmail.com>
2015-03-16 11:42:31 -0500

    Refactored GameConfiguration class

--- lib/tic_tac_toe/game_configuration.rb
+++ lib/tic_tac_toe/game_configuration.rb
@@ -1,91 +1,59 @@
 module ConsoleTTT
   class GameConfiguration
-
-    attr_reader :player1_mark, :player2_mark, :computer_opponent_mark
-
-    class ConfigValue
-      attr_reader :value, :prompt
-
-      def initialize(io, prompt)
-        @io, @prompt = io, prompt
-      end
-
-      def run!
-        @value = ""
-        @io.output(prompt)
-        input = get_input
-        if valid?(input)
-          yield value = input.to_i
-          true
-        else
-          false
-        end
-      end
-
-      private
-
-      def get_input
-        input = @io.input.chomp
-      end
-
-      def valid?(input)
-        input == "1" || input == "2"
-      end
-    end
-
+    attr_reader :player1_mark, :player2_mark, :current_player_mark, :computer_opponent_mark
 
     def initialize(io)
       @io = io
     end
 
     def setup!
-      io.output("Please choose your mark (X or O). Enter 1 for X or 2 for O:")
-      mark = get_player_mark(io)
+      mark, position, computer_opponent = collect_game_specifications_from_user
+      mark = mark == 1 ? "X" : "O"
+      @player1_mark, @player2_mark, @current_player_mark = setup_player_marks(mark, position)
+      @computer_opponent_mark = get_computer_opponent_mark(computer_opponent, mark)
+    end
 
-      io.output("Do you want to go first or second? Enter 1 or 2:")
-      position = get_player_order(io)
+    private
+    attr_reader :io
 
-      io.output("Do you want to play against the computer? Enter 1 for yes or 2 for no:")
-      @computer_opponent_mark = get_computer_opponent_mark(io, mark)
-
-      @player1_mark, @player2_mark = case position
+    def setup_player_marks(mark, position)
+      case position
       when 1
-        [mark, get_opponent(mark)]
+        [mark, get_opponent(mark), mark]
       when 2
-        [get_opponent(mark), mark]
+        [get_opponent(mark), mark, get_opponent(mark)]
       end
     end
 
-    def current_player
-      @player1_mark
-    end
-
-
-    private
-
-    attr_reader :io
-
-    def get_player_mark(io)
-      answer = io.input.chomp.to_i
-      answer == 1 ? "X" : "O"
-    end
-
-    def get_player_order(io)
-      io.input.chomp.to_i
-    end
-
-    def computer_opponent?(io)
-      answer = io.input.chomp.to_i
-    end
-
-    def get_computer_opponent_mark(input_number, mark)
-      answer = io.input.chomp.to_i
-      return nil if answer == 2
-      return get_opponent(mark)
+    def get_computer_opponent_mark(computer_opponent, mark)
+      case computer_opponent
+      when 1
+        get_opponent(mark)
+      when 2
+        nil
+      end
     end
 
     def get_opponent(mark)
       mark == "X" ? "O" : "X"
+    end
+
+
+    def collect_game_specifications_from_user
+      values = []
+      prompts.each do |prompt|
+        config_value = ConfigValue.new(io, prompt)
+        config_value.run! do |value|
+          values << value
+        end
+      end
+      values
+    end
+
+    def prompts
+      ["Please choose your mark (X or O). Enter 1 for X or 2 for O:",
+       "Do you want to go first or second? Enter 1 or 2:",
+       "Do you want to play against the computer? Enter 1 for yes or 2 for no:"]
     end
   end
 end
