880735415c95b4914708c41fad076848d4061424
Lisa Hamm <lisalhamm@gmail.com>
2015-03-17 10:12:43 -0500

    Added tests for ConfigValue class

---
+++ spec/config_value_spec.rb
@@ -0,0 +1,33 @@
+require "spec_helper"
+require "tic_tac_toe"
+
+module ConsoleTTT
+  describe ConfigValue do
+    describe "#run!" do
+      context "when input is valid" do
+        it "is true" do
+
+          io = double(InputOutput.new)
+          prompt = "Some prompt"
+          config_value = ConfigValue.new(io, prompt)
+
+          allow(io).to receive_messages(:output => "Some prompt", :input => "1")
+          expect(config_value.run!).to eq true
+        end
+      end
+
+      context "when input is invalid" do
+        it "is false" do
+
+          io = double(InputOutput.new)
+          prompt = "Some prompt"
+          config_value = ConfigValue.new(io, prompt)
+
+          allow(io).to receive_messages(:output => "Some prompt", :input => "3")
+          expect(config_value.run!).to eq false
+        end
+      end
+
+    end
+  end
+end
