1336bff16f7fe038a4bf17ae04204b3e596c8270
Lisa Hamm <lisalhamm@gmail.com>
2015-03-19 16:53:29 -0500

    Replaced strings with corresponding constants

--- spec/game_runner_spec.rb
+++ spec/game_runner_spec.rb
@@ -13,10 +13,10 @@ module ConsoleTTT
         view = double(BoardView.new(game.board, io))
         expect(view).to receive(:display).exactly(5).times
         game_runner = GameRunner.new(io, game, game_configuration, view)
-        expect(io).to receive(:output).with("Player X, it's your turn. What cell would you like to mark (1-9)?").exactly(3).times
-        expect(io).to receive(:output).with("Player O, it's your turn. What cell would you like to mark (1-9)?").exactly(3).times
-        expect(io).to receive(:output).with("The cell number you entered is invalid. Please try again.")
-        expect(io).to receive(:output).with("Game is over. Winner: Player X")
+        expect(io).to receive(:output).with("Player X" + GameRunner::TAKE_TURN).exactly(3).times
+        expect(io).to receive(:output).with("Player O" + GameRunner::TAKE_TURN).exactly(3).times
+        expect(io).to receive(:output).with(GameRunner::INVALID_CELL_INPUT)
+        expect(io).to receive(:output).with(GameRunner::GAME_OVER_WIN + "Player X")
         expect(io).to receive(:input).and_return("1", "1", "2", "4", "5", "7")
         game_runner.play!
         expect(game.in_progress?).to eq false
@@ -29,10 +29,10 @@ module ConsoleTTT
         view = double(BoardView.new(game.board, io))
         expect(view).to receive(:display).exactly(6).times
         game_runner = GameRunner.new(io, game, game_configuration, view)
-        expect(io).to receive(:output).with("Player X, it's your turn. What cell would you like to mark (1-9)?").exactly(3).times
+        expect(io).to receive(:output).with("Player X" + GameRunner::TAKE_TURN).exactly(3).times
         expect(io).to receive(:input).and_return("2", "3", "6")
         expect(game).to receive(:generate_ai_move).and_return(0, 3, 6)
-        expect(io).to receive(:output).with("Game is over. Winner: Player O")
+        expect(io).to receive(:output).with(GameRunner::GAME_OVER_WIN + "Player O")
         game_runner.play!
         expect(game.in_progress?).to eq false
       end
